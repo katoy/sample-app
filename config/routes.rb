480aa11064fa3ff5a2903ad34a7e28ffce450a4d
katoy <youichikato@gmail.com>
2021-05-02 07:33:08 +0900

    need login for tasks page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :tasks
   devise_for :users
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
@@ -7,4 +6,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: 'home#index'
+  resources :tasks
 end
