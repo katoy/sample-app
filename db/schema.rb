57ac0ffb5717b49740c92a279641c22442880780
katoy <youichikato@gmail.com>
2021-05-02 07:25:15 +0900

    After generate tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_29_092425) do
+ActiveRecord::Schema.define(version: 2021_05_01_221209) do
+
+  create_table "connections", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "task_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["task_id"], name: "index_connections_on_task_id"
+    t.index ["user_id"], name: "index_connections_on_user_id"
+  end
+
+  create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.boolean "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,4 +40,6 @@ ActiveRecord::Schema.define(version: 2021_04_29_092425) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "connections", "tasks"
+  add_foreign_key "connections", "users"
 end
---
+++ spec/factories/connections.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :connection do
+    user { nil }
+    task { nil }
+  end
+end
