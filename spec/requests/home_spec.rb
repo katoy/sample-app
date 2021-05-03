41abaf2a9a6acd2abca95704b847835c6f846c12
katoy <youichikato@gmail.com>
2021-05-03 16:20:31 +0900

    Add request spec for home

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -2,11 +2,25 @@
 
 require 'rails_helper'
 
-RSpec.describe "Homes", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/home/index"
-      expect(response).to have_http_status(:success)
+RSpec.describe 'Homes', type: :request do
+  describe 'GET /' do
+    context 'before login' do
+      it "ログイン画面が表示される" do
+        get root_path
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+
+    context 'after login' do
+      let(:user) { create :user }
+      before { sign_in user }
+
+      it "挨拶文が表示される" do
+        get root_path
+        expect(response).to have_http_status(200)
+        expect(response.body).to include 'こんにちは'
+      end
     end
   end
 end
