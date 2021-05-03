25e24e930ce46912cd6f7f873dc4e3bc1aeac13f
katoy <youichikato@gmail.com>
2021-05-03 23:09:02 +0900

    After bundle exec annotete

--- spec/factories/connections.rb
+++ spec/factories/connections.rb
@@ -1,5 +1,25 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: connections
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  task_id    :bigint           not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_connections_on_task_id  (task_id)
+#  index_connections_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (task_id => tasks.id)
+#  fk_rails_...  (user_id => users.id)
+#
 FactoryBot.define do
   factory :connection do
     user
