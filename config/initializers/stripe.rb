f120e1c9efa04aab1c0d40121101c10819ecfeaa
katoy <youichikato@gmail.com>
2021-06-13 18:55:52 +0900

    試行中

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,15 +1,8 @@
 # frozen_string_literal: true
 
-if Rails.env.test?
-  Rails.configuration.stripe = {
-      :publishable_key => 'pk_test_xxxxxxxxxxxxxxxxxxxxxxxxxx',
-      :secret_key      => 'sk_test_xxxxxxxxxxxxxxxxxxxxxxxxxx'
-  }
-else
-  Rails.configuration.stripe = {
-    publishable_key: Rails.application.credentials[:stripe][:publishable_key],
-    secret_key: Rails.application.credentials[:stripe][:secret_key]
-  }
-end
+Rails.configuration.stripe = {
+  publishable_key: Rails.application.credentials[:stripe][:publishable_key],
+  secret_key: Rails.application.credentials[:stripe][:secret_key]
+}
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
