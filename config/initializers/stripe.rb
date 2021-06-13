785754b740f7aeabc49f832ab659b57b176050dc
katoy <youichikato@gmail.com>
2021-06-13 14:48:39 +0900

    試行中

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -3,6 +3,6 @@
 Rails.configuration.stripe = {
   publishable_key: Rails.application.credentials[:stripe][:publishable_key],
   secret_key: Rails.application.credentials[:stripe][:secret_key]
-}
+} if Rails.application.credentials[:stripe]
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
