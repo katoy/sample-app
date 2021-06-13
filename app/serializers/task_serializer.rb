751c725262d5c679d45e16a84ad628f9a82efadc
katoy <youichikato@gmail.com>
2021-06-13 18:35:53 +0900

    試行中

--- app/serializers/task_serializer.rb
+++ app/serializers/task_serializer.rb
@@ -1,5 +1,20 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id         :bigint           not null, primary key
+#  name       :string(255)      not null
+#  status     :boolean          default(FALSE), not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_tasks_on_name    (name) UNIQUE
+#  index_tasks_on_status  (status)
+#
 class TaskSerializer < ActiveModel::Serializer
   attributes :id, :name, :status
 
